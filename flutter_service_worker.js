7ef9d6c972a00f168c512ed6159aaecbb469d1bc
safvan96 <safvan96@users.noreply.github.com>
2026-04-12 19:54:41 +0000

    deploy: 96065217b63de91b588307f527d3e2ff72b3c225

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.json": "f534791734053e8f0aa77523db0292d
 "canvaskit/canvaskit.js": "66177750aff65a66cb07bb44b8c6422b",
 "canvaskit/skwasm.js": "694fda5704053957c2594de355805228",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "174738dd94d4e8ac98cb8ee3ed664551",
+"flutter_bootstrap.js": "7e52f064636e81c7d0c6d688ad53a2df",
 "main.dart.js": "643b1b1871b0c7fee354479133ef3387",
 "version.json": "2252f6461351565072169c1c5f157601",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
